142798cc06d4eb08c6638d30ef1a159021af8398
Rafa <rafaelideleon@gmail.com>
2024-07-02 08:31:19 -0700

    ActionType accepts cta options

--- src/types/CustomCTAStateParam.ts
+++ src/types/CustomCTAStateParam.ts
@@ -14,8 +14,8 @@ export type CustomCTAStateParam<Initial extends CTAInitial,> = Readonly<{
 	previous: Readonly<Initial>
 	changes: Readonly<Partial<Initial>> | null
 	options?: Readonly<OptionsParams>
-	updateAction( result: Partial<Initial>, options?: ActionTypeConstructParam<Initial>['options'] ): UpdateActionType<Initial>
-	replaceAction( result: Initial, options?: ActionTypeConstructParam<Initial>['options'] ): ReplaceActionType<Initial>
-	replaceInitialAction( result: Initial, options?: ActionTypeConstructParam<Initial>['options'] ): ReplaceInitialActionType<Initial>
-	resetAction( result?: Initial, options?: ActionTypeConstructParam<Initial>['options'] ): ResetActionType<Initial>
+	updateAction( result: Partial<Initial>, options?: ActionTypeConstructParam<Initial>['actionTypeOptions'] ): UpdateActionType<Initial>
+	replaceAction( result: Initial, options?: ActionTypeConstructParam<Initial>['actionTypeOptions'] ): ReplaceActionType<Initial>
+	replaceInitialAction( result: Initial, options?: ActionTypeConstructParam<Initial>['actionTypeOptions'] ): ReplaceInitialActionType<Initial>
+	resetAction( result?: Initial, options?: ActionTypeConstructParam<Initial>['actionTypeOptions'] ): ResetActionType<Initial>
 }>;
