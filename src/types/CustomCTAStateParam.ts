f3d76e432fef5f590bf455af14b7614536b31dbc
Rafa <rafaelideleon@gmail.com>
2024-10-25 13:20:10 -0700

    Replace and ReplaceInitial Action Type

--- src/types/CustomCTAStateParam.ts
+++ src/types/CustomCTAStateParam.ts
@@ -1,7 +1,9 @@
 import {
-	createUpdateInitialActionType,
+	createReplaceActionType,
+	createReplaceInitialActionType,
 	createResetActionType,
 	createUpdateActionType,
+	createUpdateInitialActionType,
 } from '../internal/ActionTypes';
 import type { CTAInitial, } from './CTAInitial';
 import type { CTAState, } from './CTAState';
@@ -11,7 +13,9 @@ export type CustomCTAStateParam<
 	Initial extends CTAInitial,
 	Actions,
 > = CTAState<Initial> & Immutable<{
+	replaceAction: ReturnType<typeof createReplaceActionType<Initial, Actions>>
+	replaceInitialAction: ReturnType<typeof createReplaceInitialActionType<Initial, Actions>>
+	resetAction: ReturnType<typeof createResetActionType<Initial, Actions>>
 	updateAction: ReturnType<typeof createUpdateActionType<Initial, Actions>>
 	updateInitialAction: ReturnType<typeof createUpdateInitialActionType<Initial, Actions>>
-	resetAction: ReturnType<typeof createResetActionType<Initial, Actions>>
 }>;
