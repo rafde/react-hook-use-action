2b4e257d72c2fcc1dc566af344371347ef7ce93a
Rafa <rafaelideleon@gmail.com>
2024-12-02 13:29:32 -0800

    UseCTAParameter.ts

--- src/types/UseCTAParameter.ts
+++ src/types/UseCTAParameter.ts
@@ -1,17 +1,21 @@
 import { strictDeepEqual, } from 'fast-equals';
 import type { CTAInitial, } from './CTAInitial';
 
+type OnInitCallback<Initial,> = ( initial: Initial ) => Initial;
+
+type CompareCallback = ( a: unknown, b: unknown, cmp: typeof strictDeepEqual ) => boolean;
+
 export type UseCTAParameter<
 	Initial extends CTAInitial,
 	Actions,
 > = Actions extends undefined ? {
 	actions?: undefined
 	initial: Initial
-	onInit?: ( ( initial: Initial ) => Initial )
-	compare?: ( ( a: unknown, b: unknown, cmp: typeof strictDeepEqual ) => boolean )
+	onInit?: OnInitCallback<Initial>
+	compare?: CompareCallback
 } : {
 	actions: Actions
 	initial: Initial
-	onInit?: ( ( initial: Initial ) => Initial )
-	compare?: ( ( a: unknown, b: unknown, cmp: typeof strictDeepEqual ) => boolean )
+	onInit?: OnInitCallback<Initial>
+	compare?: CompareCallback
 };
