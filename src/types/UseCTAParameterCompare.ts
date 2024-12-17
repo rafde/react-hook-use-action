f7c5401c18d9ca20ac4d2d8bc857c629a5c8ef0b
Rafa <rafaelideleon@gmail.com>
2024-12-16 18:10:55 -0800

    UseCTAParameterCompare.ts change parameter to previousValue and nextValue

--- src/types/UseCTAParameterCompare.ts
+++ src/types/UseCTAParameterCompare.ts
@@ -2,8 +2,8 @@ import { strictDeepEqual, } from 'fast-equals';
 import type { CTAState, } from './CTAState';
 
 export type UseCTAParameterCompare<Initial extends CTAState,> = (
-	a: unknown,
-	b: unknown,
+	previousValue: unknown,
+	nextValue: unknown,
 	extra: {
 		cmp: typeof strictDeepEqual
 		key: keyof Initial
