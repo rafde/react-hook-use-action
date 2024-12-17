7a5c058e1bf866668e43d1cf584652830e98fb55
Rafa <rafaelideleon@gmail.com>
2024-12-17 13:47:04 -0800

    Update CustomCTAHistoryParam.ts to CustomCTAHistory.ts and defined action callbacks with explicit definition

--- src/types/CTAHistory.ts
+++ src/types/CTAHistory.ts
@@ -1,9 +1,9 @@
 import type { CTAState, } from './CTAState';
 
 export type CTAHistory<Initial extends CTAState,> = Readonly<{
-	initial: Readonly<Initial>
-	previousInitial: Readonly<Initial> | null
 	current: Readonly<Initial>
 	previous: Readonly<Initial> | null
 	changes: Readonly<Partial<Initial>> | null
+	initial: Readonly<Initial>
+	previousInitial: Readonly<Initial> | null
 }>;
