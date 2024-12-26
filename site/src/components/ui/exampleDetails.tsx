503816126c5051b11a26f74f3c7f797562b4f5ca
Rafa <rafaelideleon@gmail.com>
2024-12-26 14:24:49 -0800

    gh-page improve document ready by rendering example code iframe after clicking details open

--- site/src/components/ui/exampleDetails.tsx
+++ site/src/components/ui/exampleDetails.tsx
@@ -1,4 +1,5 @@
-import { ReactNode, } from 'react';
+'use client';
+import { ReactNode, useCallback, useEffect, useRef, useState, } from 'react';
 
 type ExampleDetailsProps = {
 	summary: ReactNode
@@ -12,17 +13,30 @@ export default function ExampleDetails( props: ExampleDetailsProps, ) {
 		src,
 		title,
 	} = props;
+	const [
+		isOpen,
+		setIsOpen,
+	] = useState( false, );
+	const openRef = useRef( isOpen, );
+	const onClick = useCallback( () => {
+		setIsOpen( isOpen => !isOpen, );
+	}, [], );
+	useEffect( () => {
+		if ( isOpen ) {
+			openRef.current = true;
+		}
+	}, [isOpen,], );
 
-	return <details className="pb-2" open={true}>
+	return <details className="pb-2" open={isOpen} onClick={onClick}>
 		<summary className="py-4 font-extrabold">
 			{summary}
 		</summary>
-		<iframe
+		{openRef.current && <iframe
 			src={src}
 			className="h-[500px] w-full overflow-hidden rounded-s border-0"
 			title={title}
 			allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
 			sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-		/>
+		/>}
 	</details>;
 }
