5ddf0e00f2bc524c628ea42b3d0098daa5ab2230
Rafa <rafaelideleon@gmail.com>
2024-12-26 19:26:27 -0800

    navOverlayOpenButton.tsx mt match navToggleButton.tsx

--- site/src/components/ui/navOverlayOpenButton.tsx
+++ site/src/components/ui/navOverlayOpenButton.tsx
@@ -5,7 +5,7 @@ import useNav from '../../hooks/useNav';
 export default function NavOverlayOpenButton() {
 	const openNavOverlay = useNav( state => state.openNavOverlay, );
 
-	return <button aria-label="Open Navigation" className="fixed z-20 ml-1 mt-3.5 bg-black sm:hidden" onClick={openNavOverlay} type="button">
+	return <button aria-label="Open Navigation" className="fixed z-20 ml-1 mt-4 bg-black sm:hidden" onClick={openNavOverlay} type="button">
 		<PanelLeftOpen />
 	</button>;
 }
