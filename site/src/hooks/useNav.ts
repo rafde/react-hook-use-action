f70775eecbaa6b9df02ebdee80b6dc09dd70252e
Rafa <rafaelideleon@gmail.com>
2024-12-26 19:27:49 -0800

    useNav.ts open sidebar when opening nav overlay close sidebar when closing nav overlay close nav overlay when closing sidebar

--- site/src/hooks/useNav.ts
+++ site/src/hooks/useNav.ts
@@ -11,18 +11,27 @@ type UseNavState = {
 const useNav = create<UseNavState>( set => ( {
 	isNavOverlayOpen: false,
 	isNavOpen: true,
-	toggleNavOverlay: () => set( state => ( {
-		isNavOverlayOpen: !state.isNavOverlayOpen,
-	} ), ),
 	openNavOverlay: () => set( () => ( {
 		isNavOverlayOpen: true,
+		isNavOpen: true,
 	} ), ),
 	closeNavOverlay: () => set( () => ( {
 		isNavOverlayOpen: false,
+		isNavOpen: false,
 	} ), ),
-	toggleNav: () => set( state => ( {
-		isNavOpen: !state.isNavOpen,
-	} ), ),
+	toggleNav: () => set( ( state, ) => {
+		const isNavOpen = !state.isNavOpen;
+
+		if ( !isNavOpen ) {
+			return {
+				isNavOverlayOpen: false,
+				isNavOpen,
+			};
+		}
+		return {
+			isNavOpen,
+		};
+	}, ),
 } ), );
 
 export default useNav;
