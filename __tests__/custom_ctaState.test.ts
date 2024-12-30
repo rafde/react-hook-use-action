86b91b5cd08e0e7d535a550bd48953f044bebbff
Rafa <rafaelideleon@gmail.com>
2024-12-30 10:26:11 -0800

    move useCTA.test.ts for custom actions to custom_ctaState.test.ts

--- __tests__/custom_ctaState.test.ts
+++ __tests__/custom_ctaState.test.ts
@@ -3,134 +3,258 @@ import { returnCTAParameter, useCTA, } from '../src';
 import { initial, } from './setup/simple';
 
 describe( 'custom action with ctaState', () => {
-	test( 'should update', () => {
-		const { result, } = renderHook( () => useCTA( {
-			initial,
-			actions: {
-				val( state, payload: string, ) {
-					return state.updateAction( {
-						test2: payload,
-					}, );
+	describe( 'actions.val(CTAHistory, string)', () => {
+		test( 'should update', () => {
+			const { result, } = renderHook( () => useCTA( {
+				initial,
+				actions: {
+					val( state, payload: string, ) {
+						return state.updateAction( {
+							test2: payload,
+						}, );
+					},
 				},
-			},
-		}, ), );
-		const test2 = 'payload';
-		const changes = {
-			test2: test2.trim(),
-		};
-		const state = {
-			...initial,
-			...changes,
-		};
+			}, ), );
+			const test2 = 'payload';
+			const changes = {
+				test2: test2.trim(),
+			};
+			const state = {
+				...initial,
+				...changes,
+			};
 
-		act( () => {
-			result.current[ 1 ].cta.val( test2, );
-		}, );
-
-		expect( result.current[ 0 ].current, ).toStrictEqual( state, );
-		expect( result.current[ 0 ].previous, ).toStrictEqual( initial, );
-		expect( result.current[ 0 ].initial, ).toStrictEqual( initial, );
-		expect( result.current[ 0 ].previousInitial, ).toBeNull( );
-		expect( result.current[ 0 ].changes, ).toStrictEqual( changes, );
-
-		const calcCTADispatchState = result.current[ 1 ].history;
-		act( () => {
-			result.current[ 1 ]( {
-				type: 'val',
-				payload: test2,
+			act( () => {
+				result.current[ 1 ].cta.val( test2, );
 			}, );
+
+			expect( result.current[ 0 ].current, ).toStrictEqual( state, );
+			expect( result.current[ 0 ].previous, ).toStrictEqual( initial, );
+			expect( result.current[ 0 ].initial, ).toStrictEqual( initial, );
+			expect( result.current[ 0 ].previousInitial, ).toBeNull( );
+			expect( result.current[ 0 ].changes, ).toStrictEqual( changes, );
+
+			const calcCTADispatchState = result.current[ 1 ].history;
+			act( () => {
+				result.current[ 1 ]( {
+					type: 'val',
+					payload: test2,
+				}, );
+			}, );
+
+			expect( calcCTADispatchState === result.current[ 1 ].history, ).toBe( true, );
 		}, );
 
-		expect( calcCTADispatchState === result.current[ 1 ].history, ).toBe( true, );
+		test( 'should update initial', () => {
+			const { result, } = renderHook( () => useCTA( {
+				initial,
+				actions: {
+					val( state, payload: string, ) {
+						return state.updateInitialAction( {
+							test2: payload,
+						}, );
+					},
+				},
+			}, ), );
+			const test2 = 'payload';
+			const changes = {
+				test2,
+			};
+			const state = {
+				...initial,
+				...changes,
+			};
+			const initialChanges = {
+				test2: initial.test2,
+			};
+
+			act( () => {
+				result.current[ 1 ].cta.val( test2, );
+			}, );
+
+			expect( result.current[ 0 ].current, ).toStrictEqual( initial, );
+			expect( result.current[ 0 ].previous, ).toBeNull( );
+			expect( result.current[ 0 ].initial, ).toStrictEqual( state, );
+			expect( result.current[ 0 ].previousInitial, ).toBe( initial, );
+			expect( result.current[ 0 ].changes, ).toStrictEqual( initialChanges, );
+
+			const calcCTADispatchState = result.current[ 1 ].history;
+			act( () => {
+				result.current[ 1 ]( {
+					type: 'val',
+					payload: test2,
+				}, );
+			}, );
+
+			expect( calcCTADispatchState === result.current[ 1 ].history, ).toBe( true, );
+		}, );
+
+		test( 'should resetCurrent using payload', () => {
+			const { result, } = renderHook( () => useCTA( {
+				initial,
+				actions: {
+					val( state, payload: string, ) {
+						return state.resetAction( {
+							...state.current,
+							test2: payload,
+						}, );
+					},
+				},
+			}, ), );
+			const test2 = 'payload';
+			const changes = {
+				test2,
+			};
+			const state = {
+				...initial,
+				...changes,
+			};
+
+			act( () => {
+				result.current[ 1 ].cta.val( test2, );
+			}, );
+
+			expect( result.current[ 0 ].current, ).toStrictEqual( state, );
+			expect( result.current[ 0 ].previous, ).toBe( initial, );
+			expect( result.current[ 0 ].initial, ).toStrictEqual( state, );
+			expect( result.current[ 0 ].previousInitial, ).toBe( initial, );
+			expect( result.current[ 0 ].changes, ).toBeNull( );
+
+			const calcCTADispatchState = result.current[ 1 ].history;
+			act( () => {
+				result.current[ 1 ]( {
+					type: 'val',
+					payload: test2,
+				}, );
+			}, );
+
+			expect( calcCTADispatchState === result.current[ 1 ].history, ).toBe( true, );
+		}, );
 	}, );
 
-	test( 'should update initial', () => {
-		const { result, } = renderHook( () => useCTA( {
-			initial,
-			actions: {
-				val( state, payload: string, ) {
-					return state.updateInitialAction( {
-						test2: payload,
-					}, );
-				},
-			},
-		}, ), );
-		const test2 = 'payload';
-		const changes = {
-			test2,
-		};
-		const state = {
-			...initial,
-			...changes,
-		};
+	describe( 'actions.val(CTAHistory, CTAState["hi"])', () => {
 		const initialChanges = {
-			test2: initial.test2,
+			there: 'you',
+			you: 'me?',
+			2: 2,
 		};
+		const initial = {
+			...initialChanges,
+			hi: 1,
+		};
+		describe( 'dispatch({type: "val", payload: unknown})', () => {
+			test( 'should add to `hi`', function() {
+				const { result, } = renderHook( () => useCTA( {
+					initial,
+					actions: {
+						val( state, payload: Pick<typeof initial, 'hi'>['hi'], ) {
+							return {
+								hi: state.current.hi + payload,
+							};
+						},
+					},
+				}, ), );
 
-		act( () => {
-			result.current[ 1 ].cta.val( test2, );
-		}, );
-
-		expect( result.current[ 0 ].current, ).toStrictEqual( initial, );
-		expect( result.current[ 0 ].previous, ).toBeNull( );
-		expect( result.current[ 0 ].initial, ).toStrictEqual( state, );
-		expect( result.current[ 0 ].previousInitial, ).toBe( initial, );
-		expect( result.current[ 0 ].changes, ).toStrictEqual( initialChanges, );
-
-		const calcCTADispatchState = result.current[ 1 ].history;
-		act( () => {
-			result.current[ 1 ]( {
-				type: 'val',
-				payload: test2,
-			}, );
-		}, );
-
-		expect( calcCTADispatchState === result.current[ 1 ].history, ).toBe( true, );
-	}, );
-
-	test( 'should resetCurrent using payload', () => {
-		const { result, } = renderHook( () => useCTA( {
-			initial,
-			actions: {
-				val( state, payload: string, ) {
-					return state.resetAction( {
-						...state.current,
-						test2: payload,
+				const payload = 4;
+				act( () => {
+					result.current[ 1 ]( {
+						type: 'val',
+						payload,
 					}, );
-				},
-			},
-		}, ), );
-		const test2 = 'payload';
-		const changes = {
-			test2,
-		};
-		const state = {
-			...initial,
-			...changes,
-		};
+				}, );
+				expect( result.current[ 0 ].current, ).toEqual( {
+					...initial,
+					hi: initial.hi + payload,
+				}, );
+				expect( result.current[ 0 ].changes, ).toEqual( {
+					hi: initial.hi + payload,
+				}, );
+			}, );
 
-		act( () => {
-			result.current[ 1 ].cta.val( test2, );
-		}, );
+			test( 'should not add to `hi` if payload is not a number', function() {
+				const { result, } = renderHook( () => useCTA( {
+					initial,
+					actions: {
+						val( state, payload: Pick<typeof initial, 'hi'>['hi'], ) {
+							if ( typeof payload !== 'number' ) {
+								return;
+							}
+							return {
+								hi: state.current.hi + payload,
+							};
+						},
+					},
+				}, ), );
 
-		expect( result.current[ 0 ].current, ).toStrictEqual( state, );
-		expect( result.current[ 0 ].previous, ).toBe( initial, );
-		expect( result.current[ 0 ].initial, ).toStrictEqual( state, );
-		expect( result.current[ 0 ].previousInitial, ).toBe( initial, );
-		expect( result.current[ 0 ].changes, ).toBeNull( );
+				const payload = 's';
+				act( () => {
+					result.current[ 1 ]( {
+						type: 'val',
+						// @ts-expect-error make sure payload is not used by calc when payload is forced
+						payload,
+					}, );
+				}, );
+				expect( result.current[ 0 ].current, ).toEqual( initial, );
+				expect( result.current[ 0 ].changes, ).toBeNull( );
+			}, );
 
-		const calcCTADispatchState = result.current[ 1 ].history;
-		act( () => {
-			result.current[ 1 ]( {
-				type: 'val',
-				payload: test2,
+			test( 'should not add to `hi` when payload is a function that returns a string', function() {
+				const { result, } = renderHook( () => useCTA( {
+					initial,
+					actions: {
+						val( state, payload: Pick<typeof initial, 'hi'>['hi'], ) {
+							if ( typeof payload !== 'number' ) {
+								return;
+							}
+							return {
+								hi: state.current.hi + payload,
+							};
+						},
+					},
+				}, ), );
+
+				const payload = () => 'sdd';
+				act( () => {
+					result.current[ 1 ]( {
+						type: 'val',
+						// @ts-expect-error make sure payload is not used by calc when payload is forced
+						payload,
+					}, );
+				}, );
+				expect( result.current[ 0 ].current, ).toEqual( initial, );
+				expect( result.current[ 0 ].changes, ).toBeNull( );
 			}, );
 		}, );
 
-		expect( calcCTADispatchState === result.current[ 1 ].history, ).toBe( true, );
+		describe( 'dispatch.cta.val(unknown)', function() {
+			test( 'should add to `hi`', function() {
+				const { result, } = renderHook( () => useCTA( {
+					initial,
+					actions: {
+						val( state, payload: Pick<typeof initial, 'hi'>['hi'], ) {
+							return {
+								hi: state.current.hi + payload,
+							};
+						},
+					},
+				}, ), );
+
+				const payload = 4;
+				act( () => {
+					result.current[ 1 ].cta.val( payload, );
+				}, );
+				expect( result.current[ 0 ].current, ).toEqual( {
+					...initial,
+					hi: initial.hi + payload,
+				}, );
+				expect( result.current[ 0 ].changes, ).toEqual( {
+					hi: initial.hi + payload,
+				}, );
+			}, );
+		}, );
 	}, );
 
-	describe( 'options', () => {
+	describe( 'actions.val(CTAHistory, string, boolean?, string?)', () => {
 		const params = returnCTAParameter( {
 			initial,
 			actions: {
@@ -191,7 +315,7 @@ describe( 'custom action with ctaState', () => {
 			expect( calcCTADispatchState === result.current[ 1 ].history, ).toBe( true, );
 		}, );
 
-		test( 'should update with trim with dispatch.cta.val(string, boolean)', () => {
+		test( 'should update with trim with dispatch.cta.val(string, boolean?)', () => {
 			const { result, } = renderHook( () => useCTA( params, ), );
 			const test2 = '      payload      ';
 			const changes = {
@@ -224,7 +348,7 @@ describe( 'custom action with ctaState', () => {
 			expect( calcCTADispatchState === result.current[ 1 ].history, ).toBe( true, );
 		}, );
 
-		test( 'should update initial with dispatch.cta.val(string, boolean, "initial")', () => {
+		test( 'should update initial with dispatch.cta.val(string, boolean?, "initial")', () => {
 			const { result, } = renderHook( () => useCTA( params, ), );
 			const test2 = 'payload';
 			const changes = {
@@ -263,7 +387,7 @@ describe( 'custom action with ctaState', () => {
 			expect( calcCTADispatchState === result.current[ 1 ].history, ).toBe( true, );
 		}, );
 
-		test( 'should resetCurrent using payload with dispatch.cta.val(string, boolean, "resetCurrent")', () => {
+		test( 'should resetCurrent using payload with dispatch.cta.val(string, boolean?, "resetCurrent")', () => {
 			const { result, } = renderHook( () => useCTA( params, ), );
 			const test2 = 'payload';
 			const changes = {
@@ -300,7 +424,7 @@ describe( 'custom action with ctaState', () => {
 		}, );
 	}, );
 
-	describe( 'required parameters', () => {
+	describe( 'actions.setter(CTAHistory, number, string, boolean)', () => {
 		const params = returnCTAParameter( {
 			initial,
 			actions: {
@@ -352,6 +476,272 @@ describe( 'custom action with ctaState', () => {
 			expect( result.current[ 0 ].previousInitial, ).toBeNull( );
 			expect( result.current[ 0 ].changes, ).toBeNull( );
 			expect( result.current[ 0 ], ).toStrictEqual( oldHistory, );
+		}, );
+	}, );
+
+	describe( 'actions.calc(CTAHistory, CTAState["hi"], {ignoreNegatives: boolean, bbb?: number}?)', function() {
+		const initialChanges = {
+			there: 'you',
+			you: 'me?',
+			2: 2,
+		};
+		const initial = {
+			...initialChanges,
+			hi: 1,
+		};
+		const params = returnCTAParameter( {
+			initial,
+			actions: {
+				calc(
+					state,
+					payload: Pick<typeof initial, 'hi'>,
+					options?: {
+						ignoreNegatives: boolean
+						bbb?: number
+					},
+				) {
+					const {
+						hi,
+					} = payload;
+
+					if ( options?.ignoreNegatives ) {
+						return;
+					}
+
+					if ( typeof hi !== 'number' ) {
+						return;
+					}
+
+					return {
+						hi: state.current.hi + hi,
+					};
+				},
+			},
+		}, );
+
+		describe( 'dispatch({type: "calc", payload: unknown}})', () => {
+			test( 'should `calc` `hi`', function() {
+				const { result, } = renderHook( () => useCTA( {
+					initial,
+					actions: {
+						calc(
+							state,
+							payload: Pick<typeof initial, 'hi'>,
+							options?: {
+								ignoreNegatives: boolean
+								bbb?: number
+							},
+						) {
+							const {
+								hi,
+							} = payload;
+
+							if ( options?.ignoreNegatives ) {
+								return;
+							}
+
+							if ( typeof hi !== 'number' ) {
+								return;
+							}
+
+							return {
+								hi: state.current.hi + hi,
+							};
+						},
+					},
+				}, ), );
+
+				const payload = {
+					hi: 4,
+				};
+				act( () => {
+					result.current[ 1 ]( {
+						type: 'calc',
+						payload,
+					}, );
+				}, );
+				expect( result.current[ 0 ].current, ).toEqual( {
+					...initial,
+					hi: initial.hi + payload.hi,
+				}, );
+				expect( result.current[ 0 ].changes, ).toEqual( {
+					hi: initial.hi + payload.hi,
+				}, );
+			}, );
+
+			test( 'should not `calc` `hi`', function() {
+				const { result, } = renderHook( () => useCTA( params, ), );
+
+				const payload = {
+					hi: '',
+				};
+				act( () => {
+					result.current[ 1 ]( {
+						type: 'calc',
+						// @ts-expect-error make sure payload is not used by calc when payload is forced
+						payload,
+					}, );
+				}, );
+
+				expect( result.current[ 0 ].current === initial, ).toBe( true, );
+				expect( result.current[ 0 ].current, ).toEqual( initial, );
+				expect( result.current[ 0 ].changes, ).toBeNull( );
+			}, );
+
+			test( 'should `calc` `hi` when `payload` is function', function() {
+				const { result, } = renderHook( () => useCTA( params, ), );
+
+				const payload = {
+					hi: 4,
+				};
+				act( () => {
+					result.current[ 1 ]( {
+						type: 'calc',
+						payload,
+					}, );
+				}, );
+				expect( result.current[ 0 ].current, ).toEqual( {
+					...initial,
+					hi: initial.hi + payload.hi,
+				}, );
+				expect( result.current[ 0 ].changes, ).toEqual( {
+					hi: initial.hi + payload.hi,
+				}, );
+			}, );
+
+			test( 'should not `calc` when `payload` is negative and `options.ignoreNegatives` === true', function() {
+				const { result, } = renderHook( () => useCTA( params, ), );
+
+				const payload = {
+					hi: -4,
+				};
+				act( () => {
+					result.current[ 1 ]( {
+						type: 'calc',
+						payload,
+						args: [
+							{
+								ignoreNegatives: true,
+							},
+						],
+					}, );
+				}, );
+				expect( result.current[ 0 ].current, ).toEqual( {
+					...initial,
+					hi: initial.hi,
+				}, );
+				expect( result.current[ 0 ].changes, ).toBeNull( );
+			}, );
+		}, );
+		describe( 'dispatch.cta.calc( unknown )', function() {
+			test( 'should `calc` `hi`', function() {
+				const { result, } = renderHook( () => useCTA( params, ), );
+
+				const val = {
+					hi: 4,
+				};
+				act( () => {
+					result.current[ 1 ].cta.calc( val, );
+				}, );
+				expect( result.current[ 0 ].current, ).toEqual( {
+					...initial,
+					hi: initial.hi + val.hi,
+				}, );
+				expect( result.current[ 0 ].changes, ).toEqual( {
+					hi: initial.hi + val.hi,
+				}, );
+			}, );
+
+			test( 'should not `calc` `hi`', function() {
+				const { result, } = renderHook( () => useCTA( params, ), );
+
+				const payload = {
+					there: '',
+				};
+				act( () => {
+					// @ts-expect-error make sure payload is not used by calc when payload is forced
+					result.current[ 1 ].cta.calc( payload, );
+				}, );
+
+				expect( result.current[ 0 ].current === initial, ).toBe( true, );
+				expect( result.current[ 0 ].current, ).toEqual( initial, );
+				expect( result.current[ 0 ].changes, ).toBeNull( );
+			}, );
+
+			test( 'should not `calc` when `payload` is negative and `options.ignoreNegatives` === true', function() {
+				const { result, } = renderHook( () => useCTA( params, ), );
+
+				const payload = {
+					hi: -4,
+				};
+				act( () => {
+					result.current[ 1 ].cta.calc( payload, {
+						ignoreNegatives: true,
+					}, );
+				}, );
+				expect( result.current[ 0 ].current, ).toEqual( {
+					...initial,
+					hi: initial.hi,
+				}, );
+				expect( result.current[ 0 ].changes, ).toBeNull( );
+			}, );
+		}, );
+	}, );
+
+	describe( 'actions.doubleHi(CTAHistory)', function() {
+		const initialChanges = {
+			there: 'you',
+			you: 'me?',
+			2: 2,
+		};
+		const initial = {
+			...initialChanges,
+			hi: 1,
+		};
+		const params = returnCTAParameter( {
+			initial,
+			actions: {
+				double( state, ) {
+					return {
+						hi: state.current.hi * 2,
+					};
+				},
+			},
+		}, );
+
+		describe( 'dispatch({type: "doubleHi"}})', () => {
+			test( 'should double `hi`', function() {
+				const { result, } = renderHook( () => useCTA( params, ), );
+
+				act( () => {
+					result.current[ 1 ]( {
+						type: 'double',
+					}, );
+				}, );
+				expect( result.current[ 0 ].current, ).toEqual( {
+					...initial,
+					hi: initial.hi * 2,
+				}, );
+				expect( result.current[ 0 ].changes, ).toEqual( {
+					hi: initial.hi * 2,
+				}, );
+			}, );
+		}, );
+		describe( 'dispatch.double', function() {
+			test( 'should double `hi`', function() {
+				const { result, } = renderHook( () => useCTA( params, ), );
+
+				act( () => {
+					result.current[ 1 ].cta.double();
+				}, );
+				expect( result.current[ 0 ].current, ).toEqual( {
+					...initial,
+					hi: initial.hi * 2,
+				}, );
+				expect( result.current[ 0 ].changes, ).toEqual( {
+					hi: initial.hi * 2,
+				}, );
+			}, );
 		}, );
 	}, );
 }, );
