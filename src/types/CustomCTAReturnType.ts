aa2fb68547433eda129889c55aada14f3fe8fbc1
Rafa <rafaelideleon@gmail.com>
2025-01-13 00:22:50 -0800

    CustomCTAReturnType.ts eslint fix

--- src/types/CustomCTAReturnType.ts
+++ src/types/CustomCTAReturnType.ts
@@ -10,9 +10,9 @@ import type { CTAState, } from './CTAState';
 export type CustomCTAReturnType<
 	Initial extends CTAState,
 > = undefined |
-ReplaceActionType<Initial> |
-ReplaceInitialActionType<Initial> |
-ResetActionType<Initial> |
-UpdateActionType<Initial> |
-UpdateInitialActionType<Initial> |
-Partial<Initial>;
+	ReplaceActionType<Initial> |
+	ReplaceInitialActionType<Initial> |
+	ResetActionType<Initial> |
+	UpdateActionType<Initial> |
+	UpdateInitialActionType<Initial> |
+	Partial<Initial>;
