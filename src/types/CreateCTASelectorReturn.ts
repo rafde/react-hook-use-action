7b3bdec6bfd27c5724329e201eb86dee4565d5ed
Rafa <rafaelideleon@gmail.com>
2025-01-14 09:56:00 -0800

    UseCTASelectorListener.ts to CTASelector.ts

--- src/types/CreateCTASelectorReturn.ts
+++ src/types/CreateCTASelectorReturn.ts
@@ -3,13 +3,13 @@ import type { CTAState, } from './CTAState';
 
 import { UseCTAParameterCreateFuncReturnRecord, } from './UseCTAParameterCreateFuncReturnRecord';
 import { UseCTAReturnTypeDispatch, } from './UseCTAReturnTypeDispatch';
-import { UseCTASelectorListener, } from './UseCTASelectorListener';
+import { CTASelector, } from './CTASelector';
 
 export type CreateCTASelectorReturn<
 	Initial extends CTAState,
 	Actions,
 	FR extends UseCTAParameterCreateFuncReturnRecord,
-> = ( <SelectorReturn,>( selector: UseCTASelectorListener<Initial, Actions, FR, SelectorReturn> ) => SelectorReturn ) & {
+> = ( <SelectorReturn,>( selector: CTASelector<Initial, Actions, FR, SelectorReturn> ) => SelectorReturn ) & {
 	dispatch: UseCTAReturnTypeDispatch<Initial, Actions, FR, void>
 	getHistory: () => CTAHistory<Initial>
 };
