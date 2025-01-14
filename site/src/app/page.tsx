35c11fbd21a921a93881911a19e86746787a5e41
Rafa <rafaelideleon@gmail.com>
2025-01-14 09:59:12 -0800

    useSourceCode.ts takes results of getSourceFiles.ts

--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -5,12 +5,14 @@ import InstallTopic from '../components/topics/install';
 import IntroductionTopic from '../components/topics/introduction';
 import ReturnCTAParameterTopic from '../components/topics/return-cta-parameter';
 import UseCTATopic from '../components/topics/use-cta';
+import SourceCodeRecord from '../components/ui/source-code-record';
 import getSourceFiles from '../util/getSourceFiles';
 
 export default async function Home() {
 	const sourceCodeRecord = await getSourceFiles();
 	return (
 		<>
+			<SourceCodeRecord sourceCodeRecord={sourceCodeRecord} />
 			<IntroductionTopic />
 			<section className="z-10 flex flex-col bg-black">
 				<InstallTopic />
