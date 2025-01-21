5365bbbbc70182e366d545829a8a36ac83e7582f
Rafa <rafaelideleon@gmail.com>
2025-01-21 10:03:53 -0800

    NestedPartial.ts stricter check on what can be a partial

--- src/types/NestedPartial.ts
+++ src/types/NestedPartial.ts
@@ -1,5 +1,5 @@
-export type NestedPartial<T,> = {
-	[P in keyof T]?: T[P] extends object
+export type NestedPartial<T,> = T extends Record<string | number | symbol, unknown> ? {
+	[P in keyof T]?: T[P] extends Record<string | number | symbol, unknown>
 		? NestedPartial<T[P]>
 		: T[P];
-};
+} : T;
